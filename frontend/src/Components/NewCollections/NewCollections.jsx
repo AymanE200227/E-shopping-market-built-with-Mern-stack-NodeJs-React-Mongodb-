3ea53044d96c30efeec912214dff3a12e9273f3d
AymanE200227 <nomdutilisateur2q@gmail.com>
2025-01-10 18:57:01 +0100

    Mapping on the data and styling our component

--- frontend/src/Components/NewCollections/NewCollections.jsx
+++ frontend/src/Components/NewCollections/NewCollections.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './NewCollections.css'
+import data from '../Assets/all_products2'
+import Item from '../Items/Item'
+
 const NewCollections = () => {
   return (
     <div className="newcollections">
-
+        <h1>NEW COLLECTIONS</h1>
+        <hr />
+        <div className="collections">
+            {data.map((item,i)=>{
+               return <Item key={i} id={item.id} name={item.name} image={item.image} category={item.category}  new_price={item.new_price} old_price={item.old_price}/>
+            })}
+        </div>
     </div>
   )
 }
